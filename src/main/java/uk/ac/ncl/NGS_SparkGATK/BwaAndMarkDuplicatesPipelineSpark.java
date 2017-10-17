8486c2523ec78825d7ccdd0970c4561e67350df5
Nicholas <nich.tucci@gmail.com>
2017-10-17 15:46:55 +0100

    BwaAndMarkDuplicatesPipelineSpark parallel - raw version

--- src/main/java/uk/ac/ncl/NGS_SparkGATK/BwaAndMarkDuplicatesPipelineSpark.java
+++ src/main/java/uk/ac/ncl/NGS_SparkGATK/BwaAndMarkDuplicatesPipelineSpark.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -14,8 +17,12 @@ import org.apache.spark.api.java.JavaSparkContext;
 /**
  * Created by Nicholas
  */
-public class BwaAndMarkDuplicatesPipelineSpark {
+public class BwaAndMarkDuplicatesPipelineSpark implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String gatkPath;
 	private String inputFolder;		//folder that contains the samples (converted in uBAM in the previous step) to process
 	private String referenceFolder;
@@ -28,16 +35,18 @@ public class BwaAndMarkDuplicatesPipelineSpark {
 	
 	public void run(JavaSparkContext sc) {
 		File folder = new File(inputFolder);
-		File[] listOfFiles = folder.listFiles();
-		
-		JavaRDD<File> ubams = sc.parallelize(Arrays.asList(listOfFiles));
+		List<File> listOfFiles = Arrays.asList(folder.listFiles()).parallelStream()
+				.filter(file -> file.getName().endsWith("_fastqtosam.bam")).collect(Collectors.toList());
+
+		JavaRDD<File> ubams = sc.parallelize(listOfFiles);
 		createBashScript();
 		
 		
 		JavaRDD<String> bashExec = ubams.map(ubam -> 
-		this.gatkPath + "|" + ubam.getName() + "|" + getReferenceFile() + "|" + ubam.getName().replaceAll("fastqtosam.bam", "dedup_reads.bam"))
+		this.gatkPath + "|" + ubam.getAbsolutePath() + "|" + getReferenceFile() + "|" + ubam.getAbsolutePath().replaceAll("fastqtosam.bam", "dedup_reads.bam"))
 				.pipe(System.getProperty("user.dir") + "/BwaAndMarkDuplicatesPipelineSpark.sh");
-		
+//		/data/ngs/libraries/gatk_4/gatk-launch|PFC_0029_IUH_AGTTCC_L007_R_fastqtosam.bam|ucsc.hg19.fasta|PFC_0029_IUH_AGTTCC_L007_R_dedup_reads.bam
+
 		//TODO execute bash commands and create Abstract Class
 		for (String string : bashExec.collect()) 
 			System.out.println(string);
@@ -50,11 +59,24 @@ public class BwaAndMarkDuplicatesPipelineSpark {
 
 	
 	private void createBashScript() {
-		String bashScript = "#!/bin/bash\n" +
+		/*String bashScript = "#!/bin/bash\n" +
+				"cd /\n" + 
 				"while read LINE; do\n" + 
 				"        IFS='|' read -a files <<< \"$LINE\"\n" + 
-				"		 ./data/ngs/libraries/gatk_4/gatk-launch BwaAndMarkDuplicatesPipelineSpark --input /data0/execution_gatk/PFC_0030_fastqtosam.bam --reference /data/ngs/reference/hg19-ucsc/ucsc.hg19.fasta --disableSequenceDictionaryValidation true --output /data0/execution_gatk/PFC_0030_dedup_reads.bam" +
-				"		 .${files[0]} BwaAndMarkDuplicatesPipelineSpark --input ${files[1]} --reference ${files[2]} --disableSequenceDictionaryValidation true --output ${files[3]}" +
+				"        ./data/ngs/libraries/gatk_4/gatk-launch BwaAndMarkDuplicatesPipelineSpark --input /data0/NGS-SparkGATK/output/PFC_0029_IUH_AGTTCC_L007_R_fastqtosam.bam --reference /data/ngs/reference/hg19-ucsc/ucsc.hg19.fasta --disableSequenceDictionaryValidation true --output /data0/NGS-SparkGATK/output/PFC_0029_IUH_AGTTCC_L007_R_dedup_reads.bam\n" + 
+				"done\n";*/
+		String bashScript = "#!/bin/bash\n" +
+				"cd /\n" + 
+				"while read LINE; do\n" + 
+//				"        echo $LINE\n" + 
+				"        IFS='|' read -a files <<< \"$LINE\"\n" + 
+//				"        echo ${files[0]}\n" + 
+//				"        echo ${files[1]}\n" + 
+//				"        echo ${files[2]}\n" + 
+//				"        echo ${files[3]}\n" + 
+				
+//				"		 ./data/ngs/libraries/gatk_4/gatk-launch BwaAndMarkDuplicatesPipelineSpark --input /data0/execution_gatk/PFC_0030_fastqtosam.bam --reference /data/ngs/reference/hg19-ucsc/ucsc.hg19.fasta --disableSequenceDictionaryValidation true --output /data0/execution_gatk/PFC_0030_dedup_reads.bam" +
+				"        .${files[0]} BwaAndMarkDuplicatesPipelineSpark --input ${files[1]} --reference ${files[2]} --disableSequenceDictionaryValidation true --output ${files[3]}\n" +
 				"done\n";
 		//${files[0]}
 
@@ -74,7 +96,7 @@ public class BwaAndMarkDuplicatesPipelineSpark {
 		
 		for (File file : reference.listFiles()) 
 			if(file.getName().endsWith(".fasta"))
-				return file.getName();
+				return file.getAbsolutePath();
 		
 		System.err.println("Unespected: .fasta file not present");
 
--- src/main/java/uk/ac/ncl/NGS_SparkGATK/Pipeline.java
+++ src/main/java/uk/ac/ncl/NGS_SparkGATK/Pipeline.java
@@ -21,10 +21,18 @@ public class Pipeline {
 	private String outFolder;
 
 
-	public Pipeline(String picardPath, String inFiles, String outFile) {
+	//testing FastqToSam
+	/*public Pipeline(String picardPath, String inFiles, String outFile) {
 		this.picardPath = picardPath;
 		this.inFiles = inFiles;
 		this.outFolder = outFile;
+	}*/
+	
+	//testing BwaAndMarkDuplicatesPipelineSpark
+	public Pipeline(String gatkPath, String inFolder, String referenceFolder) {
+		this.gatkPath = gatkPath;
+		this.inFiles = inFolder;
+		this.referenceFolder = referenceFolder;
 	}
 	
 	public Pipeline(String picardPath, String gatkPath, String inFiles, String referenceFolder, String knownSites, String outFolder) {
@@ -48,7 +56,7 @@ public class Pipeline {
 	public static void main(String[] args) {
 		double startTime = System.currentTimeMillis();
 
-		if(args.length < 6)
+		/*if(args.length < 6)
 			System.err.println("Usage:\n <picard-path> <gatk-path> <path-input-file1>,<path-input-file2> <reference-folder> "
 					+ "<known-sites1>,<known-sites2>,<known-sites3> <output-folder>");
 
@@ -56,7 +64,11 @@ public class Pipeline {
 		if(ca.check()) {
 			Pipeline pipeline = new Pipeline(args);
 			pipeline.run();
-		}
+		}*/
+		
+		Pipeline pipeline = new Pipeline(args[0], args[1], args[2]);
+		pipeline.run();
+		
 
 		double stopTime = System.currentTimeMillis();
 		double elapsedTime = (stopTime - startTime) / 1000;
@@ -67,10 +79,11 @@ public class Pipeline {
 		SparkConf conf = new SparkConf().setAppName(this.getClass().getName());
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		
-		FastqToSam fts = new FastqToSam(picardPath, inFiles, outFolder);
-		fts.run(sc);
+		/*FastqToSam fts = new FastqToSam(picardPath, inFiles, outFolder);
+		fts.run(sc);*/
 		
-		BwaAndMarkDuplicatesPipelineSpark bwa_markDuplicates = new BwaAndMarkDuplicatesPipelineSpark(this.gatkPath, this.outFolder, this.referenceFolder);
+		//testing BwaAndMarkDuplicatesPipelineSpark
+		BwaAndMarkDuplicatesPipelineSpark bwa_markDuplicates = new BwaAndMarkDuplicatesPipelineSpark(this.gatkPath, this.inFiles, this.referenceFolder);
 		bwa_markDuplicates.run(sc);
 
 		//JavaPairRDD<String, String> ubam = sc.wholeTextFiles(outFolder);
